7ee37f4d196858d87ac4b9e3d76edb7856b95b41
Michael Pope <map7@corsairsolutions.com.au>
2014-02-10 22:01:27 +1100

    Add error messages if config file doesn't exist

--- temperature_upload.rb
+++ temperature_upload.rb
@@ -9,12 +9,18 @@ agent = Mechanize.new
 page =agent.get('http://localhost:3000/users/sign_in')
 
 # Get user details
-string = IO.read("/home/map7/.winesite")
+begin
+  string = IO.read("#{ENV['HOME']}/.winesite")
+rescue
+  puts "Please create #{ENV['HOME']}/.winesite as a json file with email & password"
+  exit
+end
+
 json=JSON.parse(string)
 
 # Login to my winesite
 login_form = page.form("login")
-login_form.field_with(type: "email").value = json["username"]
+login_form.field_with(type: "email").value = json["email"]
 login_form.field_with(type: "password").value = json["password"]
 page = agent.submit(login_form)
 
