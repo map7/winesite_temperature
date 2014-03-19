2772798691f5cdbd098e7887abc1be814172fd54
Michael Pope <map7@corsairsolutions.com.au>
2014-03-19 21:58:02 +1100

    Clean up the code into methods

--- temperature_upload.rb
+++ temperature_upload.rb
@@ -5,42 +5,49 @@
 # Use mechanize to login and give me access to posting data on the site.
 require 'mechanize'
 
-# Change details to suit
-HOST="http://localhost:3000"
-CELLAR_ID="3"
-
 # Setup Mechanize
-agent = Mechanize.new
-page =agent.get('http://localhost:3000/users/sign_in')
+@agent = Mechanize.new
 
 # Get user details
-begin
-  string = IO.read("#{ENV['HOME']}/.winesite")
-rescue
-  puts "Please create #{ENV['HOME']}/.winesite as a json file with email & password"
-  exit
+def get_user_details
+  begin
+    string = IO.read("#{ENV['HOME']}/.winesite")
+  rescue
+    puts "Please create #{ENV['HOME']}/.winesite as a json file with email & password"
+    exit
+  end
+
+  # Convert the file into a JSON object which we can easily read
+  json=JSON.parse(string)
+  json
 end
 
-# Convert the file into a JSON object which we can easily read
-json=JSON.parse(string)
+# Login to the remote site
+def login(settings)
+  page =@agent.get("#{settings['url']}/users/sign_in")
 
-# Login to my winesite
-login_form = page.form("login")
-login_form.field_with(type: "email").value = json["email"]
-login_form.field_with(type: "password").value = json["password"]
-page = agent.submit(login_form)
+  # Login to my winesite
+  login_form = page.form("login")
+  login_form.field_with(type: "email").value = settings["email"]
+  login_form.field_with(type: "password").value = settings["password"]
+  page = @agent.submit(login_form)
+end
 
-# page = agent.get("#{HOST}/wines.json")
+settings = get_user_details
+login(settings)
+
+# page = @agent.get("#{settings['url']}/wines.json")
 # pp page.body
 
 params = {
           temperature: {
-            cellar_id: CELLAR_ID,
+            cellar_id: settings["cellar"],
             date: Time.now,
-            temperature: 25,
+            temperature: 20,
             humidity: 20
           }
          }
 headers = { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
-#agent.post("#{HOST}/temperatures.json", '{"temperature":{"temperature": "29"}}', headers)
-agent.post("#{HOST}/temperatures.json", params.to_json, headers)
+
+#@agent.post("#{settings['url']}/temperatures.json", '{"temperature":{"temperature": "29"}}', headers)
+@agent.post("#{settings['url']}/temperatures.json", params.to_json, headers)
